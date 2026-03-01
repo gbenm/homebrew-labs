47a666d8c80500730e20d4e8dd5811ca743c8285
Benyamin Galeano <benyamin.galeano@gmail.com>
2026-03-01 02:22:33 -0600

    Switch fire to v0.1.1 release binaries

--- Formula/fire.rb
+++ Formula/fire.rb
@@ -1,13 +1,30 @@
 class Fire < Formula
   desc "CLI with dynamic completion powered by external configuration"
   homepage "https://github.com/gbenm/fire"
+  version "0.1.1"
   license "MIT"
-  head "https://github.com/gbenm/fire.git", branch: "main"
 
-  depends_on "rust" => :build
+  on_macos do
+    on_arm do
+      url "https://github.com/gbenm/fire/releases/download/v#{version}/fire-v#{version}-macos-arm64.tar.gz"
+      sha256 "dca6258cfb95ede6dc3a1b4d32c20a9c2c1447fdb12dd1ce0d98607a918212ba"
+    end
+
+    on_intel do
+      url "https://github.com/gbenm/fire/releases/download/v#{version}/fire-v#{version}-macos-intel.tar.gz"
+      sha256 "fe6627cc159831492cecf45cf7dfb9c2bdddbca04270314917b9615c4ce876bc"
+    end
+  end
+
+  on_linux do
+    on_intel do
+      url "https://github.com/gbenm/fire/releases/download/v#{version}/fire-v#{version}-linux-x86_64.tar.gz"
+      sha256 "b26e97ebcdecbb326c734668cd728cd9ba288727efc62fa645feb7587d4cead2"
+    end
+  end
 
   def install
-    system "cargo", "install", *std_cargo_args(path: ".")
+    bin.install "fire"
   end
 
   test do
