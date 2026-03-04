4a82bc7dd1748747d1ed8b3abc51de98d66f9faf
Benyamin Galeano <benyamin.galeano@gmail.com>
2026-03-04 09:17:58 -0600

    bump fire to v2.0.2

--- Formula/fire.rb
+++ Formula/fire.rb
@@ -1,25 +1,25 @@
 class Fire < Formula
   desc "CLI with dynamic completion powered by external configuration"
   homepage "https://github.com/gbenm/fire"
-  version "2.0.1"
+  version "2.0.2"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/gbenm/fire/releases/download/v#{version}/fire-v#{version}-macos-arm64.tar.gz"
-      sha256 "b8c448d507d23c54af796494d83b939b3ebebb91991c24ad378c80d6df7a64cd"
+      sha256 "9ed013e9485a02985d4aae73bd44e075846fc58863e2d2c6d5ee44d3d519dad8"
     end
 
     on_intel do
       url "https://github.com/gbenm/fire/releases/download/v#{version}/fire-v#{version}-macos-intel.tar.gz"
-      sha256 "be1421e142034485c907e924883e2c76a2df15d72884b715f8d7d31e0504899f"
+      sha256 "dc16e29dd500bc9290ee128caf514b4210045a295756ae7e99701eb19f4f072c"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/gbenm/fire/releases/download/v#{version}/fire-v#{version}-linux-x86_64.tar.gz"
-      sha256 "960b09e5a303004f716689152bc3c08c1ae2f9ecd3c9fecaa8ab2065d2f97c94"
+      sha256 "72248d379ad9b26723cebbd35e65daddb4f64f1937ba7334855a159b3ccf9855"
     end
   end
 
